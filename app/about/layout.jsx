2b6f7e473e4131e096f9c224000a1a3cbe9741c8
MajidRaimi <imajid2000z1@gmail.com>
2023-04-25 18:04:31 +0300

    Layout ✅

---
+++ app/about/layout.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const layout = ({ children }) => {
+    return (
+        <div>
+            <h1>Layout</h1>
+            {children}
+        </div>
+    )
+}
+
+export default layout
