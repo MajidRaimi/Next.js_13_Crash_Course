e4446a80f15e336276b311683cfbd45c6c37e953
MajidRaimi <imajid2000z1@gmail.com>
2023-04-25 20:54:46 +0300

    Suspense ✅

--- app/code/repos/[name]/page.jsx
+++ app/code/repos/[name]/page.jsx
@@ -1,3 +1,4 @@
+import { Suspense } from 'react';
 import Link from 'next/link';
 import { AiOutlineArrowLeft } from 'react-icons/ai'
 import { Repo, RepoDirs } from '@/app/components'
@@ -9,8 +10,16 @@ const RepoPage = ({ params }) => {
             <Link href='/code/repos' className=''>
                 <AiOutlineArrowLeft className='m-4 hover:color-indigo-500 duration-300' />
             </Link>
-            <Repo name={name} />
-            <RepoDirs name={name} />
+
+
+            <div className='max-w-lg mx-auto'>
+                <Suspense fallback={<div className='text-center'></div>}>
+                    <Repo name={name} />
+                </Suspense>
+                <Suspense fallback={<div className=''>Loading Files And Directory</div>}>
+                    <RepoDirs name={name} />
+                </Suspense>
+            </div>
         </div>
 
 
--- app/components/RepoDirs.jsx
+++ app/components/RepoDirs.jsx
@@ -9,12 +9,14 @@ const RepoDirs = async ({ name }) => {
     const content = await fetchRepoContent(name);
     // const dirs = content.filter((content) => content.type === '');
 
+    // wait for more 3 seconds
+    await new Promise((resolve) => setTimeout(resolve, 3000));
 
     return (
         <div className='mx-auto max-w-lg'>
-            <h3 className='text-center py-2 text-3xl'>Directories</h3>
+            <h3 className='text-center py-2 text-xl'>Directories</h3>
             <ul>
-                {content.map((file, index) => (
+                {content?.map((file, index) => (
                     <li key={index} className=''>{file.type === 'dir' ? '📁' : '📃'} {file.name}</li>
                 ))}
             </ul>
