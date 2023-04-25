ca9fde867add9faac5fda3373f6c68939b09c12a
MajidRaimi <imajid2000z1@gmail.com>
2023-04-25 18:03:42 +0300

    Setup & Routing ✅

---
+++ app/about/team/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const TeamPage = () => {
+    return (
+        <div>
+            <h1>Our Team</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt ipsum tempore voluptas saepe dolorem ut expedita, asperiores dolor. Animi rem distinctio omnis nostrum ad natus quo, impedit magni voluptas eaque! Pariatur recusandae assumenda molestiae libero officiis maxime incidunt maiores voluptates aspernatur nam eligendi alias praesentium nemo ut eaque, rem animi.</p>
+        </div>
+    )
+}
+
+export default TeamPage
